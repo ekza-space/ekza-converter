5e891006c9d131fb131e3d593c2e780ecee2d125
Dmitry Sobolev <sobolev@wotori.local>
2020-05-30 14:53:12 +0300

    developing iteration.

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, request
 from flask_uploads import UploadSet, configure_uploads, All
+# import requests
 import subprocess
 
 app = Flask(__name__)
 
 fbx_files = UploadSet('fbx', All)
-app.config['UPLOAD_FBX_DIST'] = 'fbx_files'
+app.config['UPLOADED_FBX_DEST'] = 'fbx_files'
 configure_uploads(app, fbx_files)
 
-@app.route('converter')
+@app.route('/converter')
 def converter():
+
     if request.method is 'POST' and 'thefile' in request.files:
         # convert file to gltf
+        print('file recived')
         subprocess.run(args=["ls ~/"], shell=True)
 
+    return "<h1> page is working </h1>"
+
 if __name__ =='__main__':
-    app.run(debug=True, host="127.0.0.2")
+    app.run(debug=True, host="localhost", port=8081)
