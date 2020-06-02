cfdb17c81a98c07c53c52dd7cfe25059c65a8abc
Dmitry Sobolev <sobolev@wotori-mac.local>
2020-06-02 22:14:09 +0300

    save glb file

--- app.py
+++ app.py
@@ -19,15 +19,19 @@ def converter():
         fbx_file = request.files['fbx_file']
         fbx_file_name = fbx_files.save(fbx_file)
 
-        converter_path = '~/tools/FBX2glTF/FBX2glTF-darwin-x64'
-
         # convert file to gltf
+        converter_path = '~/tools/FBX2glTF/FBX2glTF-darwin-x64'
+        filename = "sphere.fbx"
         shell_command = f'{converter_path} --binary \
-            --input ./static/cloud/fbx_files/sphere.fbx \
-            --output ./static/cloud/gltf_files/'
-
+            --input ./static/cloud/fbx_files/{filename} \
+            --output ./static/cloud/gltf_files/sphere.glb'
         subprocess.run(args=shell_command, shell=True)
         print('file converted')
+
+        # send result back
+        url = 'http://localhost:8080/converter'
+        files = {'fbx_file': open('static/cloud/gltf_files/sphere.glb', 'rb')}
+        requests.post(url, files=files, data={'instructions' : 'for_database'})
 
         return f"<h1> file recived: {fbx_file_name} </h1>"
     return "<h1> page is working </h1>"
