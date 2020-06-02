5de83fc9696fe5088b4a5ed5309bc460943928ef
Dmitry Sobolev <sobolev@wotori-mac.local>
2020-06-02 21:22:56 +0300

    basic converter interface

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from flask import Flask, request
-from flask_uploads import UploadSet, configure_uploads, All
-# import requests
-import subprocess
+from flask_uploads import UploadSet, configure_uploads, ALL
+
+import requests
+import subprocess, shlex
 
 app = Flask(__name__)
 
-fbx_files = UploadSet('fbx', All)
-app.config['UPLOADED_FBX_DEST'] = 'fbx_files'
+# setting up uploader
+fbx_files = UploadSet('fbx', ALL)
+app.config['UPLOADED_FBX_DEST'] = 'static/cloud/fbx_files'
 configure_uploads(app, fbx_files)
 
-@app.route('/converter')
+@app.route('/converter', methods=['GET', 'POST'])
 def converter():
 
-    if request.method is 'POST' and 'thefile' in request.files:
-        # convert file to gltf
-        print('file recived')
-        subprocess.run(args=["ls ~/"], shell=True)
+    if request.method == 'POST':
+        # saving the file
+        fbx_file = request.files['fbx_file']
+        fbx_file_name = fbx_files.save(fbx_file)
 
+        converter_path = '~/tools/FBX2glTF/FBX2glTF-darwin-x64'
+
+        # convert file to gltf
+        shell_command = f'{converter_path} --binary \
+            --input ./static/cloud/fbx_files/sphere.fbx \
+            --output ./static/cloud/gltf_files/'
+
+        subprocess.run(args=shell_command, shell=True)
+        print('file converted')
+
+        return f"<h1> file recived: {fbx_file_name} </h1>"
     return "<h1> page is working </h1>"
 
 if __name__ =='__main__':
-    app.run(debug=True, host="localhost", port=8081)
+    app.run(debug=True, port=8081)
